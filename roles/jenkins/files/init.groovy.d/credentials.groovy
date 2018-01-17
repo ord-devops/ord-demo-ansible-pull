a598dd966313ee473efc34586d2437f6149e1e52
Wouter Hummelink <wouter.hummelink@ordina.nl>
2018-01-17 18:06:01 +0100

    Add job dsl and github token script

--- roles/jenkins/files/init.groovy.d/credentials.groovy
+++ roles/jenkins/files/init.groovy.d/credentials.groovy
@@ -6,7 +6,11 @@ import com.cloudbees.plugins.credentials.impl.*;
 String keyfile="/var/lib/jenkins/kubernetes.pfx"
 String password="jenkins"
 
+// Kubernetes keypair
 def ksm1 = new CertificateCredentialsImpl.FileOnMasterKeyStoreSource(keyfile)
 Credentials ck1 = new CertificateCredentialsImpl(CredentialsScope.GLOBAL,"kubernetes-certkey", "CN=Jenkins", password, ksm1)
 SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), ck1)
 
+// Github token
+Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,"jenkins-github-token", "Jenkins github token", "wouterhummelink", env['JENKINS_GITHUB_TOKEN'])
+SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
