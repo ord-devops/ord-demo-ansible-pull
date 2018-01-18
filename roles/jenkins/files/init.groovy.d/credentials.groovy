8880777572ebb350a0c808844869fe75b428f1e2
Wouter Hummelink <wouter.hummelink@ordina.nl>
2018-01-18 13:24:13 +0100

    Handle some exceptions for credentials

--- roles/jenkins/files/init.groovy.d/credentials.groovy
+++ roles/jenkins/files/init.groovy.d/credentials.groovy
@@ -7,10 +7,17 @@ String keyfile="/var/lib/jenkins/kubernetes.pfx"
 String password="Jenkins"
 
 // Kubernetes keypair
-def ksm1 = new CertificateCredentialsImpl.FileOnMasterKeyStoreSource(keyfile)
-Credentials ck1 = new CertificateCredentialsImpl(CredentialsScope.GLOBAL,"kubernetes-certkey", "CN=Jenkins", password, ksm1)
-SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), ck1)
-
+try {
+  def ksm1 = new CertificateCredentialsImpl.FileOnMasterKeyStoreSource(keyfile)
+  Credentials ck1 = new CertificateCredentialsImpl(CredentialsScope.GLOBAL,"kubernetes-certkey", "CN=Jenkins", password, ksm1)
+  SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), ck1)
+} catch (e) {
+  System.out.println(e.getMessage())
+}
 // Github token
-Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,"jenkins-github-token", "Jenkins github token", "wouterhummelink", System.getenv('JENKINS_GITHUB_TOKEN'))
-SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
+try {
+  Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,"jenkins-github-token", "Jenkins github token", "wouterhummelink", System.getenv('JENKINS_GITHUB_TOKEN'))
+  SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
+} catch (e) {
+  System.out.println(e.getMessage())
+}
