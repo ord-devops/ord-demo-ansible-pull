2ac525388f9d9dc5a9822ad7d55a251241901498
Wouter Hummelink <wouter.hummelink@ordina.nl>
2018-01-20 12:43:13 +0100

    Add docker podTemplate in global config

--- roles/jenkins/files/init.groovy.d/kubernetes.groovy
+++ roles/jenkins/files/init.groovy.d/kubernetes.groovy
@@ -1,16 +1,18 @@
 import org.csanchez.jenkins.plugins.kubernetes.*
+import org.csanchez.jenkins.plugins.kubernetes.volumes.*
 import jenkins.model.*
 import java.io.*
 import java.util.Collections
+
 
 def j = Jenkins.getInstance()
 
 def k = new KubernetesCloud(
   'kubernetes',
   null,
-  System.getenv('KUBERNETES_MASTER'),
+  'https://192.168.100.10:6443',
   'default',
-  System.getenv('JENKINS_ADDRESS'),
+  'http://192.168.100.20:8080/',
   '10', 0, 0, 5
 )
 k.setServerCertificate(new File('/var/lib/jenkins/kubernetes-ca.crt').text)
@@ -28,6 +30,22 @@ p = new PodTemplate('centos:7', Collections.emptyList())
 p.setName('centos7')
 p.setLabel('centos7-docker')
 p.setRemoteFs('/home/jenkins')
+
+k.addTemplate(p)
+
+def volumes = new ArrayList<PodVolume>()
+volumes.add(new HostPathVolume ('/var/run/docker.sock', '/var/run/docker.sock'))
+// volumes.add(new SecretVolume ('/home/jenkins/.kube', 'kube-config'))
+
+p = new PodTemplate('docker','docker', volumes )
+p.setName('docker')
+p.setLabel('docker')
+p.setRemoteFs('/home/jenkins')
+
+def containerTemplate = p.getFirstContainer()
+if (containerTemplate != null) {
+  containerTemplate.setTtyEnabled(true)
+}
 
 k.addTemplate(p)
 
